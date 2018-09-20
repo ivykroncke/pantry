da3654c17a7e067001a18944dfde34308c7bcebd
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-20 10:28:08 -0400

    the / route directs to the index route

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
+//WEEK ROUTER, etc.
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,7 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', (indexRouter));
+// app.use for Weeks 
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
