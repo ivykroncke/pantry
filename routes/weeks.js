1021259ea1287f3f058d6730277290ac443de6c8
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-20 14:19:27 -0400

    Created route to show one week and handlebars on the index page.

--- routes/weeks.js
+++ routes/weeks.js
@@ -2,11 +2,19 @@ var express = require('express')
 var router = express.Router()
 const { Week } = require('../db/schema')
 
-/* GET users listing. */
+//INDEX, SHOW ALL
 router.get('/', (req, res, next) => {
   Week.find()
   .then(week => {
     res.render('weeks/index', { week })
+  })
+})
+
+//INDEX, SHOW ONE
+router.get('/:id', (req, res) => {
+  Week.findById(req.params.id)
+  .then(week => {
+    res.render('weeks/show', { week })
   })
 })
 
