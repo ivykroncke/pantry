da2a5b39fc8428fd4e4c2cade66471e47cd28e5e
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-20 11:47:14 -0400

    Files seeded nested correctly but delete functiona t end of seeds file is not clearing out the seeds with each refresh.

--- db/seeds.js
+++ db/seeds.js
@@ -4,58 +4,29 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 const Schema = require('./schema')
 
-const { Week, Meal, Item } = Schema
+const { Item, Meal, Week } = Schema
 
+//ITEMS
+const tofu = new Item({ name: 'extra firm tofu', quantity: 6, unit: 'oz' })
+const spinach = new Item({ name: 'baby spinach', quantity: 1, unit: 'handful' })
+const egg = new Item({ name: 'egg', quantity: 1 })
+const pizzadough = new Item({ name: 'store bought pizza dough', quantity: 1 })
 
-const tofu = new Item({
-    name: 'extra firm tofu',
-    quantity: 6,
-    unit: 'oz' 
-})
+//MEALS
+const scramble = new Meal({ name: 'Spinach Tofu Scramble', description: 'A warm and satisfying start to your morning!', items: [tofu, spinach, egg] })
+const whitepizza = new Meal({ name: 'White Pizza with Truffle Oil', description: 'The indulgent end to your week you deserve!', items: [pizzadough, spinach] })
 
-const spinach = new Item({
-    name: 'baby spinach',
-    quantity: 1,
-    unit: 'handful'
-})
+//WEEKS
+const healthy = new Week({ name: 'Happy and Healthy', img: '#', meals: [scramble] })
+const indulgent = new Week({ name: 'Indulgent', img: '#', meals: [whitepizza] })
 
-const egg = new Item({
-    name: 'egg',
-    quantity: 1
-})
-
-const pizzadough = new Item({
-    name: 'store bought pizza dough',
-    quantity: 1
-})
-
-const scramble = new Meal({
-    name: 'Spinach Tofu Scramble',
-    description: 'A warm and satisfying start to your morning!',
-    items: [tofu, spinach, egg]
-})
-
-const whitepizza = new Meal({
-    name: 'White Pizza with Truffle Oil',
-    description: 'The indulgent end to your week you deserve!',
-    items: [pizzadough]
-})
-
-const healthy = new Week({ 
-    name: 'Happy and Healthy',
-    img: '#',
-    meals: [scramble]
-})
-
-const indulgent = new Week({
-    name: 'Indulgent',
-    img: '#',
-    meals: [whitepizza]
-})
+console.log([healthy, indulgent])
+console.log([scramble, whitepizza])
+console.log([tofu, spinach, egg, pizzadough])
 
 Meal.deleteMany()
     .then(() => {
-        return Meal.insertMany([healthy, indulgent])
+        return Week.insertMany([healthy, indulgent])
     })
     .then(() => {
         console.log('Done Seeding!')
