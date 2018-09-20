82cbc313b393130856ae095e0d2dd78c410c6dda
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-20 15:36:08 -0400

    New route and page functional and adds to database

--- routes/weeks.js
+++ routes/weeks.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 const { Week } = require('../db/schema')
 
-//INDEX, SHOW ALL
+//SHOW ALL
 router.get('/', (req, res, next) => {
   Week.find()
   .then(week => {
@@ -10,12 +10,31 @@ router.get('/', (req, res, next) => {
   })
 })
 
-//INDEX, SHOW ONE
+//NEW, RENDER NEW FORM
+router.get('/new', (req, res) => {
+  res.render('weeks/new')
+})
+
+//SHOW ONE
 router.get('/:id', (req, res) => {
   Week.findById(req.params.id)
   .then(week => {
     res.render('weeks/show', { week })
   })
 })
+
+//EDIT, RENDER EDIT FORM
+
+//CREATE
+router.post('/', (req, res) => {
+  Week.create(req.body)
+  .then(week => {
+    res.redirect(`/weeks/${week._id}`)
+  })
+})
+
+//UPDATE
+
+//DELETE
 
 module.exports = router
