cae1ad7ed8f45e14e86cb6a77da0102f11e503f4
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-20 13:44:29 -0400

    All Weeks showing on the index page.

--- routes/weeks.js
+++ routes/weeks.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const { Week } = require('../db/schema')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('weeks/index');
-});
+router.get('/', (req, res, next) => {
+  Week.find()
+  .then(week => {
+    res.render('weeks/index', { week })
+  })
+})
 
-module.exports = router;
+module.exports = router
