2517ab511da5d330cd4a1b575fc37980e4b73b64
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-21 13:56:14 -0400

    items router responds when hit

--- routes/items.js
+++ routes/items.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 const { Week } = require('../db/schema')
+
+router.get('/', (req, res) => {
+    res.send('new route!')
+    // Week.findById(req.params.weeksId)
+    //     .then(week => {
+    //         const meal = week.meals.id(req.params.weeksId),
+    //         const item = meal.items.id(req.params.id)
+    //         res.send(item)
+    //     })
+})
+
+
+module.exports = router;
