6a2176169650e9a4b3e676c62fe76c1471190009
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-22 13:47:35 -0400

    Added more robust seeding data

--- db/seeds.js
+++ db/seeds.js
@@ -11,22 +11,26 @@ const tofu = new Item({ name: 'extra firm tofu', quantity: 6, unit: 'oz' })
 const spinach = new Item({ name: 'baby spinach', quantity: 1, unit: 'handful' })
 const egg = new Item({ name: 'egg', quantity: 1 })
 const pizzadough = new Item({ name: 'store bought pizza dough', quantity: 1 })
+const tortillas = new Item({ name: 'fresh tortillas', quantity: 6})
+const porkloin = new Item({ name: 'high-quality pork loin', quantity: 12, unit: 'ounces'})
+const sweetpotato = new Item({ name: 'sweet potato', quantity: 2, unit: 'small'})
+const kale = new Item({ name: 'pre-chopped kale', quantity: 12, unite: 'ounces'})
 
 //MEALS
 const scramble = new Meal({ name: 'Spinach Tofu Scramble', description: 'A warm and satisfying start to your morning!', items: [tofu, spinach, egg] })
 const whitepizza = new Meal({ name: 'White Pizza with Truffle Oil', description: 'The indulgent end to your week you deserve!', items: [pizzadough, spinach] })
+const carnitas = new Meal({ name: 'Rustic Carnitas with Queso Fresca', description: 'A weeknight delight that won\'t go to your waistband!', items: [tortillas, porkloin]})
+const veggieplate = new Meal({ name: 'Farmer\'s Autumn Vegetable Plate', description: 'Seasonal favorites ready in less than 30 minutes!', items: [sweetpotato, kale]})
 
 //WEEKS
-const healthy = new Week({ name: 'Happy and Healthy', img: '#', meals: [scramble] })
-const indulgent = new Week({ name: 'Indulgent', img: '#', meals: [whitepizza] })
-
-console.log([healthy, indulgent])
-console.log([scramble, whitepizza])
-console.log([tofu, spinach, egg, pizzadough])
+const healthy = new Week({ name: 'Happy and Healthy', img: '/images/greensmoothie.jpg', meals: [scramble] })
+const indulgent = new Week({ name: 'Indulgent', img: '/images/mockupimage.jpeg', meals: [whitepizza] })
+const balanced = new Week({ name: 'Perfectly Balanced', img: '/images/tacos.jpeg', meals: [carnitas]})
+const veggie = new Week({ name: 'Veggie Week', img: '/images/veggieplate.jpeg', meals: [veggieplate]})
 
 Week.deleteMany()
     .then(() => {
-        return Week.insertMany([healthy, indulgent])
+        return Week.insertMany([healthy, indulgent, balanced, veggie])
     })
     .then(() => {
         console.log('Done Seeding!')
