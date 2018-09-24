a7e9abc05dd20d7328a3ed0b36b16e3fd47de0f4
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-09-24 15:53:24 -0400

    Got dynamic images to show using handlebars. Will attempt to assign images to an array in the schema instead and possibly push new images in to that array with square constrictions.

--- routes/weeks.js
+++ routes/weeks.js
@@ -1,81 +1,76 @@
-var express = require('express')
-var router = express.Router()
-const { Week } = require('../db/schema')
+var express = require("express") 
+var router = express.Router() 
+const { Week } = require("../db/schema") 
 
 //SHOW ALL
-router.get('/', (req, res, next) => {
-  Week.find()
-  .then(week => {
-    res.render('weeks/index', { week })
-  })
-})
+router.get("/", (req, res, next) => {
+  Week.find().then(week => {
+    res.render("weeks/index", { week }) 
+  }) 
+}) 
 
 //NEW, RENDER NEW FORM
-router.get('/new', (req, res) => {
-  res.render('weeks/new')
-})
+router.get("/new", (req, res) => {
+  Week.find().then(week => {
+    res.render("weeks/new", { week }) 
+  }) 
+}) 
 
 //SHOW ONE
-router.get('/:id', (req, res) => {
-  Week.findById(req.params.id)
-  .then(week => {
-    res.render('weeks/show', { week })
-  })
-})
+router.get("/:id", (req, res) => {
+  Week.findById(req.params.id).then(week => {
+    res.render("weeks/show", { week }) 
+  }) 
+}) 
 
 //EDIT, RENDER EDIT FORM
-router.get('/:id/edit', (req, res) => {
-  Week.findById(req.params.id)
-    .then(week => {
-      res.render('weeks/edit', { week })
-    })
-})
+router.get("/:id/edit", (req, res) => {
+  Week.findById(req.params.id).then(week => {
+    res.render("weeks/edit", { week }) 
+  }) 
+}) 
 
 //LIST
-router.get('/:id/list', (req, res) => {
-  Week.findById(req.params.id)
-    .then(week => {
-        const allItems = {}
-        const allMeals = week.meals
-        allMeals.map(meal => {
-          meal.items.map(item => {
-            if (allItems[item.name]) {
-              allItems[item.name] += item.quantity
-            } else {
-              allItems[item.name] = item.quantity
-            }
-          })
-        })
-        const entries = Object.entries(allItems) 
-        res.render('weeks/list', { 
-          entries,
-          week
-        })
-    })
-})
+router.get("/:id/list", (req, res) => {
+  Week.findById(req.params.id).then(week => {
+    const allItems = {} 
+    const allMeals = week.meals 
+    allMeals.map(meal => {
+      meal.items.map(item => {
+        if (allItems[item.name]) {
+          allItems[item.name] += item.quantity 
+        } else {
+          allItems[item.name] = item.quantity 
+        }
+      }) 
+    }) 
+    const entries = Object.entries(allItems) 
+    res.render("weeks/list", {
+      entries,
+      week
+    }) 
+  }) 
+}) 
 
 //CREATE
-router.post('/', (req, res) => {
-  Week.create(req.body)
-  .then(week => {
-    res.redirect(`/weeks/${week._id}`)
-  })
-})
+router.post("/", (req, res) => {
+  Week.create(req.body).then(week => {
+    res.redirect(`/weeks/${week._id}`) 
+  }) 
+}) 
 
 //UPDATE
-router.put('/:id', (req, res) => {
-  Week.findByIdAndUpdate(req.params.id, req.body)
-  .then(week => {
-    res.redirect(`/weeks/${week._id}`)
-  })
-})
+router.put("/:id", (req, res) => {
+  Week.findByIdAndUpdate(req.params.id, req.body).then(week => {
+    res.redirect(`/weeks/${week._id}`) 
+  }) 
+}) 
 
 //DELETE
-router.delete('/:id', (req, res) => {
-  Week.findByIdAndRemove(req.params.id)
-  .then(() => {
-    res.redirect('/weeks')
-  })
-})
+router.delete("/:id", (req, res) => {
+  Week.findByIdAndRemove(req.params.id).then(() => {
+    res.redirect("/weeks") 
+  }) 
+}) 
 
-module.exports = router
+module.exports = router 
